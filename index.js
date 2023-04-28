3b8874bbfa98ba8dc7f4aedc3e4ccdf9f4766c4c
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-04-27 21:36:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(newName){
+    return cats.push(newName); 
+}
+
+function destructivelyPrependCat(newName){
+    return cats.unshift(newName);
+}
+
+function destructivelyRemoveLastCat(newName){
+    return cats.pop(newName);
+}
+
+function destructivelyRemoveFirstCat(newName){
+    return cats.shift(newName);
+}
+
+function appendCat(newName){
+    let newCats = [...cats, newName];
+    return newCats;
+}
+
+function prependCat(newName){
+    let newCats = [ newName, ...cats];
+    return newCats;
+}
+
+function removeLastCat(name){
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
